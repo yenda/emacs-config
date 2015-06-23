561072dc1ca7cf6d8ea8a7508c3368edaf5fce17
Eric Dvorsak <yenda1@gmail.com>
2015-06-23 11:05:57 +0200

    adding files

---
+++ init.el
@@ -0,0 +1,5 @@
+;; Initial emacs configuration files. We'll keep it as small as possible
+;; as the real configuration appends in an org file (litterate config.)
+
+(require 'org)
+(org-babel-load-file "~/.emacs.d/emacs.org")
